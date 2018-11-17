96acee488678233248512d683276ece050f51b39
Laura Messa Travin <laura.travin@gmail.com>
2018-11-17 01:44:43 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,5 +1,5 @@
 # Create your haunted_houses migration here
-class CreateHaunted_Houses < ActiveRecord::Migration[5.1]
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   def up
   end
   def down
