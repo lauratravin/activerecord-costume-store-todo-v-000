5fa722f9e731432b916704b8ad4451616eab3c6f
Laura Messa Travin <laura.travin@gmail.com>
2018-11-17 01:12:20 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,17 @@
 # Create your costume_stores migration here
+class CreateCostumes_stores
+  def up
+  end
+  def down
+  end
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.integer :costume_inventory
+      t.integer :number_of_employees
+      t.boolean :business
+      t.datetime :open_time
+      t.datetime :close_time
+    end
+  end
+end
