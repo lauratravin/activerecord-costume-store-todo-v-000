2e6e7c631f81962dedcc36acf8031e55fe60d3c6
Laura Messa Travin <laura.travin@gmail.com>
2018-11-17 01:15:15 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,19 @@
 # Create your haunted_houses migration here
+class CreateHaunted_houses
+  def up
+  end
+  def down
+  end
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :family
+      t.datetime :open_time
+      t.datetime :close_time
+      t.text :description
+    end
+  end
+end
